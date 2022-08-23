174fee98fb1cf41da585974dbdb2b90726886a4c
Joseph Ligier <joseph.ligier@accenture.com>
2022-08-23 16:47:24 +0200

    Fix outputs products arn

--- modules/securityhub/outputs.tf
+++ modules/securityhub/outputs.tf
@@ -1,4 +1,4 @@
 output "products_arn" {
   description = "List of product arn"
-  value       = [for p in var.enable_products : format("arn:aws:securityhub:%s:%s", data.aws_region.current.name, p)]
+  value       = [for p in var.enable_products : format("arn:aws:securityhub:%s::%s", data.aws_region.current.name, p)]
 }
